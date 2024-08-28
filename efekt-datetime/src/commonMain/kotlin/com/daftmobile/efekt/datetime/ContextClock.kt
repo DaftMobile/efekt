8583f67f32e8a1b0582a48b1031ec82af0c60a9e
Damian Baczyński <damian.baczynski@daftmobile.com>
2024-08-28 20:05:27 +0200

    Add emulatedClockContext

--- efekt-datetime/src/commonMain/kotlin/com/daftmobile/efekt/datetime/ContextClock.kt
+++ efekt-datetime/src/commonMain/kotlin/com/daftmobile/efekt/datetime/ContextClock.kt
@@ -3,12 +3,15 @@ package com.daftmobile.efekt.datetime
 import com.daftmobile.efekt.StateContext
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlin.time.TimeSource
 
 public val StateContext.clock: Clock get() = get(ContextClock)
 
 public fun Clock.toContextClock(): StateContext = if (this is ContextClock) this else ContextClock(this)
 
 public fun Instant.toContextClock(): StateContext = ConstClock(this).toContextClock()
+
+public fun emulatedClockContext(startingAt: Instant): StateContext = EmulatedClock(startingAt).toContextClock()
 
 private class ContextClock(
     private val clock: Clock,
@@ -24,4 +27,14 @@ private class ContextClock(
 private class ConstClock(private val date: Instant) : Clock {
 
     override fun now(): Instant = date
+}
+
+private class EmulatedClock(
+    private val startingAt: Instant,
+    timeSource: TimeSource = TimeSource.Monotonic
+) : Clock {
+
+    private val startMark = timeSource.markNow()
+
+    override fun now(): Instant = startingAt + startMark.elapsedNow()
 }
