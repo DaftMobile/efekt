da24910b3838b440038b2032225eb583680f58f3
Damian Baczyński <damian.baczynski@daftmobile.com>
2024-08-28 21:26:15 +0200

    Migrate publishing to vanniktech publish plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     api(libs.kotlin.plugin)
     api(libs.mokkery.plugin)
     api(libs.dokka.plugin)
+    api(libs.vanniktech.publish.plugin)
 }
--- buildSrc/src/main/kotlin/efekt-publish.gradle.kts
+++ buildSrc/src/main/kotlin/efekt-publish.gradle.kts
@@ -1,59 +1,37 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
     id("org.jetbrains.dokka")
 }
 
-
-val dokkaJar by tasks.creating(Jar::class) {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    archiveClassifier.set("javadoc")
-    from(tasks.findByName("dokkaGfm"))
-}
-
-signing {
-    sign(publishing.publications)
-}
-
-publishing {
-    val ossrhUsername: String? by project
-    val ossrhPassword: String? by project
-    repositories {
-        maven {
-            name = "sonatype"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = ossrhUsername
-                password = ossrhPassword
+mavenPublishing {
+    coordinates(project.group.toString(), project.name, project.version.toString())
+    signAllPublications()
+    publishToMavenCentral(SonatypeHost.S01, automaticRelease = false)
+    pom {
+        name.set(project.name)
+        description.set(
+            "Efekt is a Redux-like pattern implementation with effects!"
+        )
+        url.set(GitHttp)
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/mit/")
+                distribution.set("repo")
             }
         }
-    }
-    publications.withType<MavenPublication> {
-        artifact(dokkaJar)
-        pom {
-            name.set(project.name)
-            description.set(
-                "Efekt is a Redux-like pattern implementation with effects!"
-            )
+        developers {
+            developer {
+                id.set("DaftMobile")
+                name.set("DaftMobile")
+            }
+        }
+        scm {
             url.set(GitHttp)
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/license/mit/")
-                    distribution.set("repo")
-                }
-            }
-            developers {
-                developer {
-                    id.set("DaftMobile")
-                    name.set("DaftMobile")
-                }
-            }
-            scm {
-                url.set(GitHttp)
-                connection.set(GitConnection)
-                developerConnection.set(GitDevConnection)
-            }
+            connection.set(GitConnection)
+            developerConnection.set(GitDevConnection)
         }
     }
 }
